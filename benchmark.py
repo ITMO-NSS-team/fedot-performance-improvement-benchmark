2119e0c8aa9e18975f6c01d6ce8c0a63a1a61196
Sergei Pakulin <pakylin3@mail.ru>
2022-07-07 11:56:05 +0500

    upd experiments to new api

--- benchmark.py
+++ benchmark.py
@@ -76,8 +76,7 @@ def dummy_time_check():
             print(f"task_type={task_type}, mean_time={mean(repeat(check, repeat=15, number=1))}")
 
 
-def _run(timeouts: List[int], train_data: InputData, test_data: InputData, base_fedot_params: dict,
-         use_pipelines_cache: bool = False, use_preprocessing_cache: bool = False):
+def _run(timeouts: List[int], train_data: InputData, test_data: InputData, params: dict):
     times = []
     pipelines_count = []
     for timeout in timeouts:
@@ -87,16 +86,14 @@ def _run(timeouts: List[int], train_data: InputData, test_data: InputData, base_
         cache_effectiveness = collections.Counter()
         for _ in range(mean_range):
             train_data_tmp = deepcopy(train_data)
-
             start_time = timeit.default_timer()
-            auto_model = Fedot(**base_fedot_params, timeout=timeout, use_pipelines_cache=use_pipelines_cache,
-                               use_preprocessing_cache=use_preprocessing_cache)
+            auto_model = Fedot(**params, timeout=timeout)
             auto_model.fit(features=train_data_tmp)
             time += (timeit.default_timer() - start_time) / 60
             c_pipelines += _count_pipelines(auto_model.history)
-            if use_pipelines_cache and auto_model.api_composer.pipelines_cache.effectiveness_ratio:
+            if params['use_pipelines_cache'] and auto_model.api_composer.pipelines_cache.effectiveness_ratio:
                 cache_effectiveness += auto_model.api_composer.pipelines_cache.effectiveness_ratio
-            elif use_preprocessing_cache and auto_model.api_composer.preprocessing_cache.effectiveness_ratio:
+            elif params['use_preprocessing_cache'] and auto_model.api_composer.preprocessing_cache.effectiveness_ratio:
                 cache_effectiveness += auto_model.api_composer.preprocessing_cache.effectiveness_ratio
 
         time /= mean_range
@@ -119,20 +116,21 @@ def use_cache_check(problem: str, train_data, test_data, n_jobs: int = 1, test_p
     """
     pipelines_count, times = [{False: [], True: []} for _ in range(2)]
     plot_labels = {False: 'without cache', True: 'with cache'}
-    composer_params = {'with_tuning': False}
     preset = 'fast_train'
-    base_fedot_params = {
-        'problem': problem, 'seed': 42,
-        'preset': preset, 'verbose_level': logging.NOTSET, 'n_jobs': n_jobs,
-        **composer_params
+    fedot_params = {
+        'problem': problem, 'preset': preset, 'with_tuning': False,
+        'verbose_level': logging.NOTSET, 'n_jobs': n_jobs, 'seed': 42
     }
     timeouts = [1, 2, 3, 4, 5]
     for use_cache in [True, False]:
         print(f'Using cache: {use_cache}')
-        use_pipelines_cache = use_cache if not test_preprocessing else False
-        use_preprocessing_cache = False if not test_preprocessing else use_cache
-        _times, _pipelines_count = _run(timeouts, train_data, test_data, base_fedot_params, use_pipelines_cache,
-                                        use_preprocessing_cache)
+        if test_preprocessing:
+            fedot_params['use_pipelines_cache'] = False
+            fedot_params['use_preprocessing_cache'] = use_cache
+        else:
+            fedot_params['use_pipelines_cache'] = use_cache
+            fedot_params['use_preprocessing_cache'] = False
+        _times, _pipelines_count = _run(timeouts, train_data, test_data, fedot_params)
         times[use_cache] = _times
         pipelines_count[use_cache] = _pipelines_count
     _show_performance_plot(f'Cache performance with n_jobs={n_jobs}', timeouts, pipelines_count, times, plot_labels)
@@ -146,20 +144,21 @@ def compare_one_process_to_many(problem: str, train_data, test_data, n_jobs: int
     assert n_jobs != 1, 'This test uses multiprocessing, so you should have > 1 processors'
     pipelines_count, times = [{1: [], n_jobs: []} for _ in range(2)]
     plot_labels = {1: 'one process', n_jobs: f'{n_jobs} processes'}
-    composer_params = {'with_tuning': False}
-    base_fedot_params = {
-        'problem': problem, 'seed': 42,
-        'preset': 'fast_train', 'verbose_level': logging.NOTSET,
-        **composer_params
+    fedot_params = {
+        'problem': problem, 'preset': 'fast_train', 'with_tuning': False,
+        'verbose_level': logging.NOTSET, 'seed': 42
     }
     timeouts = [1, 2, 3, 4, 5]
     for _n_jobs in [1, n_jobs]:
         print(f'Processes used: {_n_jobs}')
-        use_pipelines_cache = True if not test_preprocessing else False
-        use_preprocessing_cache = False if not test_preprocessing else True
-        base_fedot_params['n_jobs'] = _n_jobs
-        _times, _pipelines_count = _run(timeouts, train_data, test_data, base_fedot_params, use_pipelines_cache,
-                                        use_preprocessing_cache)
+        if test_preprocessing:
+            fedot_params['use_pipelines_cache'] = False
+            fedot_params['use_preprocessing_cache'] = True
+        else:
+            fedot_params['use_pipelines_cache'] = True
+            fedot_params['use_preprocessing_cache'] = False
+        fedot_params['n_jobs'] = _n_jobs
+        _times, _pipelines_count = _run(timeouts, train_data, test_data, fedot_params)
         times[_n_jobs] = _times
         pipelines_count[_n_jobs] = _pipelines_count
     _show_performance_plot(f'Cache performance comparison between one process and {n_jobs}', timeouts, pipelines_count,
@@ -184,10 +183,10 @@ if __name__ == "__main__":
     examples_dct = defaultdict(lambda: (lambda: print('Wrong example number option'),))
     examples_dct.update({
         1: (dummy_time_check,),
-        2: (use_cache_check, problem, train_data, test_data, 1, True),
+        2: (use_cache_check, problem, train_data, test_data, 1, False),
         3: (compare_one_process_to_many, problem, train_data, test_data, -1, True)
     })
-    benchmark_number = 1
+    benchmark_number = 2
     func, *args = examples_dct[benchmark_number]
 
     func(*args)
